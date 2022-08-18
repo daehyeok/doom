6196a9af84d36ebbbbbba3a87e14df5fbbe653b9
Daehyeok Mun <daehyeok@gmail.com>
2022-08-18 13:17:45 -0600

    Initial commit

---
+++ config.el
@@ -0,0 +1,95 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Daehyeok Mun"
+      user-mail-address "daehyeok@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq default-input-method "korean-hangul")
+(if IS-MAC
+    (setq doom-font (font-spec :family "D2Coding Nerd Font" :size 13))
+    (setq doom-font (font-spec :family "D2Coding Nerd Font" :size 20))
+    )
+
+
+;; (setq doom-font (font-spec :family "D2Coding Nerd" :size 12))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-vibrant)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/gdrive/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+(global-visual-line-mode t)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(when IS-MAC
+    (setq mac-command-modifier 'meta
+          mac-right-option-modifier 'none
+          mac-option-modifier 'super))
+
+;; Terminal setup
+(setq xterm-extra-capabilities '(setSelection))
+
+(load-file-from-doomdir "config_work")
+
+;; vterm directory tracking
+(after! vterm
+  (add-to-list 'vterm-eval-cmds '("update-pwd" (lambda (path) (setq default-directory path))))
+  )
+
+(setq winum-keymap
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+        (define-key map (kbd "M-1") 'winum-select-window-1)
+        (define-key map (kbd "M-2") 'winum-select-window-2)
+        (define-key map (kbd "M-3") 'winum-select-window-3)
+        (define-key map (kbd "M-4") 'winum-select-window-4)
+        (define-key map (kbd "M-5") 'winum-select-window-5)
+        (define-key map (kbd "M-6") 'winum-select-window-6)
+        (define-key map (kbd "M-7") 'winum-select-window-7)
+        (define-key map (kbd "M-8") 'winum-select-window-8)
+        map))
+
+(when IS-PERSONAL
+  (use-package! org-roam
+    :init
+    (setq org-roam-directory (file-truename "~/org/roam/"))))
---
+++ init.el
@@ -0,0 +1,114 @@
+ ;;; init.el -*- lexical-binding: t; -*-
+
+;; This file controls what Doom modules are enabled and what order they load
+;; in. Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find a "Module Index" link where you'll find
+;;      a comprehensive list of Doom's modules and what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
+;;
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
+;;      directory (for easy access to its source code).
+
+;; Change leader alt key for non-evil mode
+(setq doom-leader-alt-key "M-m"
+      doom-localleader-alt-key "C-M-m")
+
+(eval-after-load 'package
+  '(add-to-list 'package-archives
+                '("melpa" . "https://melpa.org/packages/")))
+
+(eval-after-load 'package
+  '(add-to-list 'package-archives
+                '("org-melpa" . "http://orgmode.org/elpa/")))
+
+
+;; come from https://github.com/rbutoi/dotfiles/blob/master/emacs/.doom.d/init.el
+(defconst
+  GLAPTOP
+  (not (not (and IS-LINUX (string-match-p
+                           "roam" (shell-command-to-string "hostname -f"))))))
+(defconst IS-WORK ; has custom coding setup
+  (or GLAPTOP (not (not (string-match-p "google\\|penguin" (system-name))))))
+
+(defconst IS-PERSONAL (not IS-WORK))
+
+;; load file from doom directory
+(defun load-file-from-doomdir (name)
+    (load (concat (file-name-as-directory doom-private-dir) name))
+)
+
+(if IS-LINUX (load-file-from-doomdir  "init_linux"))
+
+(doom! :input
+       :completion
+       (company +childframe)             ; the ultimate code completion backend
+       (ivy +fuzzy)        ; a search engine for love and life
+
+       :ui
+       doom                ; what makes DOOM look the way it does
+       doom-dashboard      ; a nifty splash screen for Emacs
+       hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       modeline            ; snazzy, Atom-inspired modeline, plus API
+       nav-flash           ; blink cursor line after big motions
+       opnhints            ; highlight the region an operation acts on
+       (popup +all +defaults)   ; tame sudden yet inevitable temporary windows
+       treemacs            ; a project drawer, like neotree but cooler
+       unicode             ; extended unicode support for various languages
+       vc-gutter           ; vcs diff in the fringe
+       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
+       workspaces          ; tab emulation, persistence & separate workspaces
+       (window-select +numbers)
+
+       :editor
+       (:if (not IS-LINUX)
+        file-templates)
+       fold                ; (nigh) universal code folding
+       (format +on-save)
+       multiple-cursors    ; editing in many places at once
+       snippets            ; my elves. They type so I don't have to
+
+       :emacs
+       (dired +icons)      ; making dired pretty [functional]
+       electric            ; smarter, keyword-based electric-indent
+       (ibuffer +icons)    ; interactive buffer management
+       (undo +tree)        ; persistent, smarter undo for your inevitable mistakes
+       vc                  ; version-control and Emacs, sitting in a tree
+
+       :term
+       vterm               ; the best terminal emulation in Emacs
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+
+       :tools
+       (:if IS-LINUX
+        editorconfig)
+       (eval +overlay)     ; run code, run (also, repls)
+       lookup              ; navigate your code and its documentation
+       lsp
+       magit               ; a git porcelain for Emacs
+       direnv
+
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       (tty +osc)          ; improve the terminal Emacs experience
+
+       :lang
+       (go +lsp)
+       (markdown +syntax)            ; writing docs for people to ignore
+       (org +pretty +roam2)               ; organize your plain life in plain text
+       (python +lsp)
+       (sh +syntax)                  ; she sells {ba,z,fi}sh shells on the C xor
+       (yaml +syntax)                ; JSON, but readable
+       (rust +lsp)
+       (cc +lsp)
+       (kotlin +lsp)
+       (nix +lsp)
+
+       :config
+       (default +bindings +smartparens))
